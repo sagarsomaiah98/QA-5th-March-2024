cb5f4d4cc50d96ee44926fb2bac64f1d12d2bd1b
Som <somaiah1988@gmail.com>
2024-04-04 23:06:19 -0300

    java intro

---
+++ QA Class 15 Java Program Structure and Syntax/src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Java is a programming language");
+		System.out.println("Selenium is automation tool");
+		System.out.println("Test Cases");
+
+	}
+
+}
